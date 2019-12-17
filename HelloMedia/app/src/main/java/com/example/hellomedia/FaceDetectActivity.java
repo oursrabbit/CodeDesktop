583c36fa308bcac640e83ea0db6e062d6f5734b4
oursrabbit <giddyyang@gmail.com>
2019-12-17 21:07:11 +0800

    1217Night

--- HelloMedia/app/src/main/java/com/example/hellomedia/FaceDetectActivity.java
+++ HelloMedia/app/src/main/java/com/example/hellomedia/FaceDetectActivity.java
@@ -1,5 +1,6 @@
 package com.example.hellomedia;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.AlertDialog;
@@ -7,11 +8,18 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.SurfaceTexture;
 import android.hardware.camera2.CameraAccessException;
 import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraDevice;
 import android.hardware.camera2.CameraManager;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.HandlerThread;
 import android.text.InputType;
+import android.view.Surface;
+import android.view.SurfaceView;
+import android.view.TextureView;
 import android.view.View;
 import android.widget.EditText;
 
@@ -83,13 +91,69 @@ public class FaceDetectActivity extends AppCompatActivity {
         try {
             for (String cameraID: cameraManager.getCameraIdList()) {
                 CameraCharacteristics chars = cameraManager.getCameraCharacteristics(cameraID);
-                System.out.println(chars);
+                if (chars.get(CameraCharacteristics.LENS_FACING) == CameraCharacteristics.LENS_FACING_FRONT){
+                    cameraManager.openCamera(cameraID, new CameraStateCallback(), null);
+                }
             }
         } catch (CameraAccessException e) {
             e.printStackTrace();
         }
     }
 
+    private class SurfaceCallback implements TextureView.SurfaceTextureListener {
+        @Override
+        public boolean onSurfaceTextureDestroyed(SurfaceTexture surface) {
+            return false;
+        }
+
+        @Override
+        public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height) {
+
+        }
+
+        @Override
+        public void onSurfaceTextureSizeChanged(SurfaceTexture surface, int width, int height) {
+
+        }
+
+        @Override
+        public void onSurfaceTextureUpdated(SurfaceTexture surface) {
+
+        }
+    }
+
+    TextureView preView;
+
+    private class CameraStateCallback extends CameraDevice.StateCallback {
+        @Override
+        public void onOpened(@NonNull CameraDevice camera) {
+            preView = (TextureView)findViewById(R.id.textureView);
+            SurfaceTexture texture = preView.getSurfaceTexture();
+            texture.setDefaultBufferSize(128,128);
+            Surface surface = new Surface(texture);
+        }
+
+        @Override
+        public void onError(@NonNull CameraDevice camera, int error) {
+
+        }
+
+        @Override
+        public void onDisconnected(@NonNull CameraDevice camera) {
+
+        }
+    }
+    /*private class CameraStateCallback : CameraDevice.StateCallback() {
+        override fun onOpened(camera: CameraDevice) {
+            cameraDevice = camera
+            runOnUiThread { Toast.makeText(this@MainActivity, "相机已开启", Toast.LENGTH_SHORT).show() }
+        }
+
+        override fun onError(camera: CameraDevice, error: Int) {
+            camera.close()
+            cameraDevice = null
+        }
+    }*/
     private void cleanUpVedioCapture(){
 
     }
