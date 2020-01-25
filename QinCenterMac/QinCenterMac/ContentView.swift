e0d4fedc4e07ba879daf0c15e376e8d5fcb47162
oursrabbit <giddyyang@gmail.com>
2020-01-25 17:24:22 +0800

    sdf

---
+++ QinCenterMac/QinCenterMac/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  QinCenterMac
+//
+//  Created by 杨璨 on 2020/1/24.
+//  Copyright © 2020 杨璨. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
