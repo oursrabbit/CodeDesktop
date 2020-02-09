9f57ddaf51e7dec18ccf49beb9072b12e7b7f858
oursrabbit <giddyyang@gmail.com>
2020-02-09 20:55:15 +0800

    ui残

--- Qin-ios/Qin-ios/BuildingListViewController.swift
+++ Qin-ios/Qin-ios/BuildingListViewController.swift
@@ -15,6 +15,7 @@ class BuildingListViewController: StaticViewController {
     var selectedBuilding: Building!
     
     @IBOutlet weak var buildingList: UITableView!
+    @IBOutlet weak var buildingCollection: UICollectionView!
     @IBOutlet weak var studentNameLabel: UILabel!
 
     
@@ -45,6 +46,50 @@ class BuildingListViewController: StaticViewController {
         default:
             break
         }
+    }
+}
+
+extension BuildingListViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return buildings.count;
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "building", for: indexPath) as! BuildingCollectionViewCell
+        cell.initCellInterface()
+        if let image = UIImage(named: "\(buildings[indexPath.row].ID)") {
+            cell.buildingImage.image = image
+        } else {
+            cell.buildingImage.image = UIImage(named: "0")
+        }
+        cell.buildingNameLabel.text = buildings[indexPath.row].Name
+        cell.buildingRoomsLabel.text = "\(buildings[indexPath.row].Rooms.count) 间教室"
+        return cell
+    }
+}
+
+extension BuildingListViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let screenWidth = collectionView.bounds.size.width;
+        let cellWidth = (screenWidth - 32) / 2
+        return CGSize(width: cellWidth, height: cellWidth)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 16
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 8
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 8, left: 8, bottom: 8, right: 8)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        selectedBuilding = buildings[indexPath.row]
+        self.performSegue(withIdentifier: "roomlist", sender: self)
     }
 }
 
